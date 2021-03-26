3c695f4e77a1ba95467a709b6683fd5f97701f55
ab rahim <49904704+abrahimpatel@users.noreply.github.com>
2021-03-26 19:55:43 +0530

    added new logic

--- src/test/java/apitest/Test01_Get.java
+++ src/test/java/apitest/Test01_Get.java
@@ -3,15 +3,18 @@ package apitest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+//io.restassured.RestAssured.*
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class Test01_Get {
 	
 	@Test
 	void Test_01()
 	{
-		Response Res = RestAssured.get("https://reqres.in/api/users?page=2");
+		Response Res = get("https://reqres.in/api/users?page=2");
 		System.out.println("Status code:"+Res.getStatusCode());
 		System.out.println("Response Body:"+Res.getBody());
 		System.out.println("Response String:"+Res.asString());
@@ -21,6 +24,8 @@ public class Test01_Get {
 	@Test
 	void Test_02()
 	{
-	
+		given()
+		.get("https://reqres.in/api/users?page=2")
+		.then().statusCode(200);
 	}
 }
